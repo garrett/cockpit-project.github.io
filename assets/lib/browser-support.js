b1fffa206d6ea07e0b5a50049d49414ad3890c6b
Garrett LeSage <garrett@lesage.us>
2019-10-23 09:41:05 +0200

    Base browser detection on support YAML

--- assets/lib/browser-support.js
+++ assets/lib/browser-support.js
@@ -1,86 +1,54 @@
 (function (){
-    // Stub translation function
-    function _(message) {
-        return message;
+    function req(name, obj) {
+        var valid;
+
+        try {
+            valid = obj[name];
+        } catch (err) {
+            valid = undefined;
+        }
+
+        return (valid !== undefined);
     }
 
-    // Simple enough formatting
-    function format(message, reason) {
-        console && console.warn(message.replace("$0", reason));
-        console.log(reason);
+    function css(prop, val) {
+        var valid;
+
+        try {
+            valid = CSS.supports(prop, val);
+        } catch (err) {
+            valid = false;
+        }
+
+        return valid;
     }
 
-    // Simple disableLogin replacement
-    function disableLogin(name) {
-        format("Missing support for $0", name);
-    }
+    function browserCheck() {
+        var browserTest = true,
+            valid;
 
-    // Error handler
-    function fatal(message) {
-        return message;
+        {% for rule in site.data.browser_support.rules %}{% if rule[1].req %}
+        /* Check {{ rule[0] }}: {{ rule[1].req[0] }} in {{ rule[1].req[1] }} */
+        if (!req('{{ rule[1].req[0] }}', {{ rule[1].req[1] }})) browserTest = false;
+        {% endif %}{% if rule[1].css %}
+        /* Check {{ rule[0] }}: @supports({{ rule[1].css[0] }}:{{ rule[1].css[1] }}) */
+        if (!css('{{ rule[1].css[0] }}', '{{ rule[1].css[1] }}')) browserTest = false;
+        {% endif %}{% endfor %}
+
+        return browserTest;
     }
 
     // Success handler
     function success() {
-        console.log("Hooray!");
-    }
-
-    // A straight-up copy/paste of Cockpit's login.js
-    // (with just a disableLogin removed at the top)
-    function requisites() {
-        function req(name, obj) {
-            var ret;
-            try {
-                ret = (obj[name]);
-            } catch (ex) {
-                fatal(format(_("The web browser configuration prevents Cockpit from running (inaccessible $0)"), name));
-                throw ex;
-            }
-            if (ret === undefined) {
-                disableLogin(name);
-                return false;
-            }
-            return true;
-        }
-
-        function css() {
-            /*
-             * Be certain to use parenthesis when checking CSS strings
-             * as Edge is oddly particular.
-             *
-             * Instead of "display: inline", use:
-             * "(display: inline)"
-             *        or
-             * "display", "inline"
-             */
-            var args = [].join.call(arguments, ": ");
-
-            if (!window.CSS.supports.apply(this, arguments)) {
-                fatal(format(_("The web browser configuration prevents Cockpit from running (inaccessible $0)"), args));
-                disableLogin(args);
-                return false;
-            }
-            return true;
-        }
-
-        return ("MozWebSocket" in window || req("WebSocket", window)) &&
-               req("XMLHttpRequest", window) &&
-               req("sessionStorage", window) &&
-               req("JSON", window) &&
-               req("defineProperty", Object) &&
-               req("console", window) &&
-               req("pushState", window.history) &&
-               req("textContent", document) &&
-               req("CSS", window) &&
-               req("supports", window.CSS) &&
-               css("display", "flex") &&
-               css("display", "grid");
+        document
+            .getElementById('browser-support')
+            .setAttribute('data-browser-supported', true);
     }
 
     // Bind an event listener for when the page has loaded
-    document.addEventListener("DOMContentLoaded", function(event) {
-        document.getElementById("browser-supported") &&
-            requisites() &&
+    document.addEventListener('DOMContentLoaded', function(event) {
+        document.getElementById('browser-support') &&
+            browserCheck() &&
             success();
     });
 }());
